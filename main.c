2e040584f3db3a9b1eb9dfb2fa71e4b2c12d01a5
Pranav Ramesh Mannatt <pranav.mannatt@gmail.com>
2025-07-10 10:06:31 +0530

    Initial Commit

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#define size 100 //macro
+// #-preprocessor
+void main(){
+    int a=10;
+    //int main - gives successful termination,
+    //whereas void main doesn't
+    printf("Hi World\n");
+}
